0069eb2877b9a1ce4a4f1e55313b53d03fefdfe4
Flavio Lima <flavio.lima@aluno.riogrande.ifrs.edu.br>
2021-09-10 13:33:52 -0300

    Em desenvolvimento: src/Texto.java

--- src/Texto.java
+++ src/Texto.java
@@ -1,1 +1,37 @@
-// IMPLEMENTE!
+/**
+ * Texto
+ */
+
+public class Texto {
+
+// Laço de Repetição com while.
+  // public static String pad(String umaString, int largura, char umCaractere){
+
+  //   int preencher = (largura - umaString.length());
+  //   int contador = preencher-2;
+
+  //   while(contador < preencher || contador > preencher){
+  //     if (contador < preencher) {
+  //       umaString += umCaractere;
+  //       contador++;
+  //     } else {
+  //       umaString += (preencher * umCaractere);
+  //     }
+
+  //   }
+  //   return umaString;
+  // }
+
+// Laço de repetição com for i.
+  public static String pad(String umaString, int largura, char umCaractere) {
+    int preencher = largura - umaString.length();
+    // Laço de repetição: umaString == largura.
+    for (int i = 0; i < preencher; i++) {
+      umaString += umCaractere;
+    }
+    return umaString;
+  }
+
+}
+
+
