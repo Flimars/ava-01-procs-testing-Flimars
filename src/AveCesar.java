129cf186a3d0659546caf6eae69d359ae83a967d
Flavio Lima <flavio.lima@aluno.riogrande.ifrs.edu.br>
2021-09-21 15:53:37 -0300

    Em desenvolvimento faltam correções.

--- src/AveCesar.java
+++ src/AveCesar.java
@@ -3,6 +3,7 @@ class AveCesar {
   static String encrypt(String text, int key){
     String messageEncryted = "";
 
+
     for (int i = 0; i < text.length(); i++) {
       messageEncryted += (char)(text.charAt(i) + key);
     }
@@ -11,6 +12,17 @@ class AveCesar {
 
   static String decrypt(String messageEncrypted, int key) {
     String text = "";
+    messageEncrypted = messageEncrypted.toUpperCase();
+
+    if (messageEncrypted.length() > 50) {
+      System.out.println("Ultapassa o limite de 50 caracteres!");
+      return messageEncrypted = "";
+    }
+
+    if (key < 0 || key > 25 || key == 0) {
+      return messageEncrypted;
+    }
+
 
     for (int i = 0; i < messageEncrypted.length(); i++) {
       text += (char) (messageEncrypted.charAt(i) - key);
